aa45b58467f4095715100a279f3f266dcb2b4d6a
Valerio-Occhinegro <valerioocchinegro621@gmail.com>
2025-09-12 15:07:29 +0200

    changed redis token and url

--- api/redirect.js
+++ api/redirect.js
@@ -2,8 +2,8 @@ import { Redis } from "@upstash/redis";
 
 // Connessione a Upstash Redis tramite variabili ambiente
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
 
 export default async function handler(req, res) {
