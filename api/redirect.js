68b0878b282f6dd1ac85473e43cc67e0878546fd
Valerio-Occhinegro <valerioocchinegro621@gmail.com>
2025-09-12 15:13:43 +0200

    changed redis url

--- api/redirect.js
+++ api/redirect.js
@@ -2,7 +2,7 @@ import { Redis } from "@upstash/redis";
 
 // Connessione a Upstash Redis tramite variabili ambiente
 const redis = new Redis({
-  url: process.env.KV_URL,
+  url: process.env.REDIS_URL,
   token: process.env.KV_REST_API_TOKEN,
 });
 
