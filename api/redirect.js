bfedc16c12987f9b8b7bb85c04c34c2e4ca2be72
Valerio-Occhinegro <valerioocchinegro621@gmail.com>
2025-09-12 15:27:00 +0200

    changed url AGAIN

--- api/redirect.js
+++ api/redirect.js
@@ -2,13 +2,13 @@ import { Redis } from "@upstash/redis";
 
 // Connessione a Upstash Redis tramite variabili ambiente
 const redis = new Redis({
-  url: process.env.REDIS_URL,
+  url: process.env.KV_REST_API_URL,
   token: process.env.KV_REST_API_TOKEN,
 });
 
 export default async function handler(req, res) {
   //verifica i token
-  console.log("REDIS_URL:", process.env.REDIS_URL);
+  console.log("REDIS_URL:", process.env.KV_REST_API_URL);
   console.log("KV_REST_API_TOKEN:", process.env.KV_REST_API_TOKEN ? "✓ presente" : "✗ mancante");
 
   const formA = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
